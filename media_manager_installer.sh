25f53a7634671a2d74c02cf0feab5c8927a9907b
Jithin Jones Scaria <admin@jjscaria.com>
2020-07-31 04:09:26 +0000

    Update media_manager_installer.sh

--- media_manager_installer.sh
+++ media_manager_installer.sh
@@ -2,13 +2,13 @@
 # By: Jithin Scaria
 # Email: admin@jjscaria.com
 # Download this file: wget https://raw.githubusercontent.com/jjscaria/media/master/media_manager_installer.sh
-
+#
 # Download PIA from https://www.privateinternetaccess.com/helpdesk/guides/linux/install-2/linux-systemd-installing-the-pia-app
-
+#
 # Init
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
-
+#
 # Repos
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
 echo "deb http://linux.teamviewer.com/deb stable main" | sudo tee /etc/apt/sources.list.d/teamviewer.list
@@ -18,10 +18,10 @@ echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | su
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
 echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 sudo apt-get update
-
+#
 # Core Applications
 sudo apt-get install git-core python python-setuptools tzdata jq filezilla qbittorrent teamviewer -y
-
+#
 # Tautulli - http://localhost:8181
 sudo git clone https://github.com/Tautulli/Tautulli.git /opt/Tautulli/
 sudo mkdir /opt/TautulliData
@@ -33,7 +33,7 @@ wget -P /etc/opt/ https://raw.githubusercontent.com/jjscaria/media/master/config
 sudo systemctl daemon-reload
 sudo systemctl enable tautulli.service
 sudo systemctl start tautulli.service
-
+#
 # Radarr - http://localhost:7878
 sudo apt install mono-devel curl mediainfo
 curl -L -O $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
@@ -44,21 +44,21 @@ sudo systemctl daemon-reload
 sudo systemctl enable tautulli.service
 sudo systemctl start tautulli.service
 rm -f *.gz
-
+#
 # Sonarr - http://localhost:8989
 sudo apt install nzbdrone
 wget -P /etc/systemd/system/ https://raw.githubusercontent.com/jjscaria/media/master/sonarr.service
 sudo systemctl daemon-reload
 sudo systemctl enable tautulli.service
 sudo systemctl start tautulli.service
-
+#
 # Jackett - http://localhost:9117
 jackettver=$(curl https://api.github.com/repos/Jackett/Jackett/releases/latest -s | jq .name -r)
 wget https://github.com/Jackett/Jackett/releases/download/$jackettver/Jackett.Binaries.LinuxAMDx64.tar.gz
 tar -xvf Jackett*
 sudo ./Jackett/install_service_systemd.sh
 rm -f *.gz
-
+#
 # Files
 wget -P ~/.config/qBittorrent/ https://raw.githubusercontent.com/jjscaria/media/master/qBittorrent.conf
 wget -P ~/.config/filezilla/ https://raw.githubusercontent.com/jjscaria/media/master/filezilla.xml
